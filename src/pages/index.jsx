30718cfb4b9b938c530fdcc6e86bc9fbbf39e7ce
keaton.tang@gmail.com <keaton.tang@gmail.com>
2021-04-07 16:51:43 +1000

    Uploaded personal blog

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,8 +24,8 @@ const Index = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
   return (
     <Layout>
-      <Helmet title={'Home Page'} />
-      <Header title="Home Page">Gatsby Tutorial Starter</Header>
+      <Helmet title={'Keaton Tang'} />
+      <Header title="Keaton Tang">applesauce</Header>
       <PostWrapper>
         {edges.map(({ node }) => {
           const { id, excerpt, frontmatter } = node;
